856680d0719c89541c61953e40f43cadd3754e41
Abrham007 <abrhamaraya111@gmail.com>
2024-03-31 11:03:15 +0300

    finihsed and added the main footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import CharacterItem from "@/components/HomePage/CharacterItem";
+import MainFooter from "@/components/MainFooter/MainFooter";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <section>
+        <ul></ul>
+      </section>
+      <MainFooter></MainFooter>
+    </main>
+  );
 }
