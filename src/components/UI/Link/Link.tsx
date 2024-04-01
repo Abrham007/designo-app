72102c38bd36631764c69a445ef857a0d5c3d8fd
Abrham007 <abrhamaraya111@gmail.com>
2024-04-01 17:18:32 +0300

    finished a very reusabel link component

--- src/components/UI/Link/Link.tsx
+++ src/components/UI/Link/Link.tsx
@@ -6,11 +6,17 @@ export default function Link({
   title,
   imgSrc,
   large,
+  className,
 }: {
   href: string;
   title: string;
-  imgSrc: string;
+  imgSrc: {
+    desktop: { large?: string; small: string };
+    tablet: string;
+    mobile: string;
+  };
   large?: boolean;
+  className?: string;
 }) {
   const common = { alt: "" };
   const {
@@ -19,7 +25,10 @@ export default function Link({
     ...common,
     width: 541,
     height: large ? 640 : 308,
-    src: imgSrc,
+    src:
+      imgSrc.desktop.large && large
+        ? imgSrc.desktop.large
+        : imgSrc.desktop.small,
   });
   const {
     props: { srcSet: tablet },
@@ -27,7 +36,7 @@ export default function Link({
     ...common,
     width: 689,
     height: 200,
-    src: imgSrc,
+    src: imgSrc.tablet,
   });
   const {
     props: { srcSet: mobile, ...rest },
@@ -35,12 +44,12 @@ export default function Link({
     ...common,
     width: 327,
     height: 250,
-    src: imgSrc,
+    src: imgSrc.mobile,
   });
   return (
     <NextLink
       href={href}
-      className="relative h-[200px] flex flex-col gap-3 md:gap-6 justify-center items-center rounded-[15px]"
+      className={`relative w-[327px] md:w-[689px] lg:w-[541px] h-[250px] md:h-[200px] lg:h-[308px] flex flex-col gap-3 md:gap-6 justify-center items-center rounded-[15px] bg-[#00000080] hover:bg-[#E7816B80] ${className}`}
     >
       <picture>
         <source media="(min-width: 1020px)" srcSet={desktop} />
@@ -49,14 +58,14 @@ export default function Link({
         <img
           {...rest}
           alt=""
-          className="absolute top-0 bottom-0 left-0 right-0"
+          className="h-full w-full absolute top-0 bottom-0 left-0 right-0 rounded-[15px] -z-10"
         />
       </picture>
 
       <p className="text-[1.75rem] leading-[2.25rem] tracking-[0.0875rem] font-medium md:text-xl text-White uppercase">
         {title}
       </p>
-      <span className="flex gap-4 md:gap-[21px]">
+      <span className="flex gap-4 md:gap-[21px] items-center">
         <span className="text-[0.9375rem] text-White tracking-[0.3125rem] font-medium">
           VIEW PROJECTS
         </span>
