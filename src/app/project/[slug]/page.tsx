85c66f2db1cc50da8862939fcaf3edd4b3451d89
Abrham007 <abrhamaraya111@gmail.com>
2024-04-02 20:01:26 +0300

    finished the style of the page

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import MainFooter from "@/components/MainFooter/MainFooter";
 import ProjectHeader from "@/components/ProjectPage/ProjectHeader";
 import ProjectList from "@/components/ProjectPage/ProjectList";
-
+import Image from "next/image";
+import leafPattern from "../../../../public/images/shared/desktop/bg-pattern-leaf.svg";
 import ProjectData from "@/interfaces/project";
 import data from "../../../data.json";
 import ProjectLinks from "@/components/ProjectPage/ProjectLinks";
@@ -16,7 +17,12 @@ export default function Project({ params }: { params: { slug: string } }) {
   let slug = currentProject?.slug ?? "";
 
   return (
-    <main>
+    <main className="relative">
+      <Image
+        src={leafPattern}
+        alt=""
+        className="hidden lg:inline-block absolute top-[50px] left-0 -z-10 "
+      ></Image>
       <ProjectHeader title={title} description={description}></ProjectHeader>
       <section className="w-full lg:max-w-[1111px] flex flex-col gap-[120px] lg:gap-[160px] lg:mx-auto px-6 md:px-10 lg:px-0 mb-[120px] md:mb-[108px] lg:mb-[160px] ">
         <ProjectList list={projects}></ProjectList>
