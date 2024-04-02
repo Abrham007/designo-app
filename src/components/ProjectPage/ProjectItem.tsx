4bd2beda20065b530ac650aef1346689a5d52326
Abrham007 <abrhamaraya111@gmail.com>
2024-04-02 20:02:31 +0300

    improved the UX

--- src/components/ProjectPage/ProjectItem.tsx
+++ src/components/ProjectPage/ProjectItem.tsx
@@ -7,7 +7,7 @@ export default function ProjectItem({
   image,
 }: ProjectItemData) {
   return (
-    <li className="w-[327px] h-[478px] md:w-[689px] md:h-[310px] lg:w-[350px] lg:h-[478px] mx-auto flex flex-col md:flex-row lg:flex-col rounded-[15px] bg-[#FDF3F0] hover:bg-Peach">
+    <li className="group w-[327px] h-[478px] md:w-[689px] md:h-[310px] lg:w-[350px] lg:h-[478px] mx-auto flex flex-col md:flex-row lg:flex-col rounded-[15px] bg-[#FDF3F0] hover:bg-Peach">
       <Image
         src={image}
         alt={title}
@@ -16,8 +16,12 @@ export default function ProjectItem({
         className="w-[327px] md:w-[339px] lg:w-[350px] rounded-t-[15px] md:rounded-none md:rounded-l-[15px] lg:rounded-none lg:rounded-t-[15px]"
       ></Image>
       <div className="w-full h-full flex flex-col gap-4 justify-center items-center text-center">
-        <h2 className="w-[277px] text-lg text-Peach uppercase">{title}</h2>
-        <p className="w-[277px] text-base text-Black">{description}</p>
+        <h2 className="w-[277px] text-lg text-Peach group-hover:text-White uppercase">
+          {title}
+        </h2>
+        <p className="w-[277px] text-base text-Black group-hover:text-White ">
+          {description}
+        </p>
       </div>
     </li>
   );
