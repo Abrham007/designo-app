d1e492a167bc8c8e0577eacbb8f790070f9f1112
Abrham007 <abrhamaraya111@gmail.com>
2024-04-02 10:11:24 +0300

    made the code more reusable

--- src/components/HomePage/HomeLinks.tsx
+++ src/components/HomePage/HomeLinks.tsx
@@ -1,29 +1,5 @@
 import Link from "@/components/UI/Link/Link";
-
-type ImgSrcObject = {
-  [key: string]: any;
-};
-
-const imgSrcObject: ImgSrcObject = {
-  web: {
-    desktop: {
-      large: "/images/home/desktop/image-web-design-large.jpg",
-      small: "/images/home/desktop/image-web-design-small.jpg",
-    },
-    tablet: "/images/home/tablet/image-web-design.jpg",
-    mobile: "/images/home/mobile/image-web-design.jpg",
-  },
-  app: {
-    desktop: { small: "/images/home/desktop/image-app-design.jpg" },
-    tablet: "/images/home/tablet/image-app-design.jpg",
-    mobile: "/images/home/mobile/image-app-design.jpg",
-  },
-  graphic: {
-    desktop: { small: "/images/home/desktop/image-graphic-design.jpg" },
-    tablet: "/images/home/tablet/image-graphic-design.jpg",
-    mobile: "/images/home/mobile/image-graphic-design.jpg",
-  },
-};
+import { imgSrcObject } from "@/util/imgSrcObject";
 
 export default function HomeLinks() {
   return (
