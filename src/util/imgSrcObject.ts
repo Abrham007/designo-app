f338fc9dd1a4f07eb8ddcfdbcfba03a8dbe7eafc
Abrham007 <abrhamaraya111@gmail.com>
2024-04-02 10:12:47 +0300

    added a ts file for image src

---
+++ src/util/imgSrcObject.ts
@@ -0,0 +1,24 @@
+type ImgSrcObject = {
+  [key: string]: any;
+};
+
+export const imgSrcObject: ImgSrcObject = {
+  web: {
+    desktop: {
+      large: "/images/home/desktop/image-web-design-large.jpg",
+      small: "/images/home/desktop/image-web-design-small.jpg",
+    },
+    tablet: "/images/home/tablet/image-web-design.jpg",
+    mobile: "/images/home/mobile/image-web-design.jpg",
+  },
+  app: {
+    desktop: { small: "/images/home/desktop/image-app-design.jpg" },
+    tablet: "/images/home/tablet/image-app-design.jpg",
+    mobile: "/images/home/mobile/image-app-design.jpg",
+  },
+  graphic: {
+    desktop: { small: "/images/home/desktop/image-graphic-design.jpg" },
+    tablet: "/images/home/tablet/image-graphic-design.jpg",
+    mobile: "/images/home/mobile/image-graphic-design.jpg",
+  },
+};
