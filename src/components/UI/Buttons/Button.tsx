e67b9721f655eed1b8f67093ddacb8b533650ef9
Abrham007 <abrhamaraya111@gmail.com>
2024-04-04 17:12:02 +0300

    made the component more reusable

--- src/components/UI/Buttons/Button.tsx
+++ src/components/UI/Buttons/Button.tsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { ButtonHTMLAttributes } from "react";
 
-export default function Button({
-  children,
-  $type,
-}: {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   $type: string;
-}) {
+}
+
+export default function Button({ children, $type, ...props }: ButtonProps) {
   switch ($type) {
     case "1":
       return (
-        <button className="px-6 py-4 text-[0.9375rem] tracking-[0.0625rem] uppercase font-medium text-DarkGrey hover:text-White  bg-White hover:bg-LightPeach rounded-lg">
+        <button
+          {...props}
+          className="px-6 py-4 text-[0.9375rem] tracking-[0.0625rem] uppercase font-medium text-DarkGrey hover:text-White  bg-White hover:bg-LightPeach rounded-lg"
+        >
           {children}
         </button>
       );
     case "2":
       return (
-        <button className="px-6 py-4 text-[0.9375rem] tracking-[0.0625rem] uppercase font-medium text-White hover:text-White bg-Peach hover:bg-LightPeach rounded-lg">
+        <button
+          {...props}
+          className="px-6 py-4 text-[0.9375rem] tracking-[0.0625rem] uppercase font-medium text-White hover:text-White bg-Peach hover:bg-LightPeach rounded-lg"
+        >
           {children}
         </button>
       );
     default:
       return (
-        <button className="px-6 py-4 text-[0.9375rem] tracking-[0.0625rem] uppercase font-medium text-DarkGrey hover:text-White  bg-White hover:bg-LightPeach rounded-lg">
+        <button
+          {...props}
+          className="px-12 py-4 text-[0.9375rem] tracking-[0.0625rem] uppercase font-medium text-DarkGrey hover:text-White  bg-White hover:bg-LightPeach rounded-lg"
+        >
           {children}
         </button>
       );
