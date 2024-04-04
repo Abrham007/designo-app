eb11f7993913b81d0c65632cea738b0478af1368
Abrham007 <abrhamaraya111@gmail.com>
2024-04-04 17:11:09 +0300

    finsihed the header for the contact page

---
+++ src/components/ContactPage/ContactHeader.tsx
@@ -0,0 +1,15 @@
+export default function ContactHeader() {
+  return (
+    <header className="lg:w-[445px] flex flex-col gap-6 text-center md:text-start px-6 md:px-[58px] lg:px-0 pt-[72px] lg:pt-0 bg-smallCircles bg-[right_-50px_center] bg-contain md:bg-none">
+      <h1 className="text-[2rem] leading-[2.25rem] font-medium text-White md:text-xl2">
+        Contact Us
+      </h1>
+      <p className="text-sm text-White md:text-base">
+        Ready to take it to the next level? Let’s talk about your project or
+        idea and find out how we can help your business grow. If you are looking
+        for unique digital experiences that’s relatable to your users, drop us a
+        line.
+      </p>
+    </header>
+  );
+}
